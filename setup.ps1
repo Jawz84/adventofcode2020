729c6d1d17dc633462a950767c6dcaab24868fa6
Jos Koelewijn <jos.koelewijn@cloudnation.nl>
2020-12-02 10:47:12 +0100

    add empty folders and files

--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,4 @@
-$parentDirectoryName = (Split-Path $pwd -Leaf)
-if ($parentDirectoryName -notmatch "^adventofcode\d{4}$") {
-    Write-Error "Parent dir must be named 'adventofcodeYYYY' where YYYY holds a number indicating the year."
-}
-
-for ($i = 1; $i -le 25; $i++) {
-    $day = "day$i"
-    
-    $folderToCreate = Join-Path -Path $pwd -ChildPath $day
-    New-Item -Path $folderToCreate -ItemType Directory
-    
-    $filesToCreate = "$day-1.ps1", "$day-2.ps1", "input.ps1" | ForEach-Object { Join-Path -path $pwd -ChildPath $_ }
-    $filesToCreate | ForEach-Object {
-        New-Item -Path $_ -ItemType File
-    }
-    
-    $fileContents = Get-Content $filesToCreate[0]
-    if ([string]::IsNullOrEmpty($fileContents)) {
-        Set-Content -Path $filesToCreate -Value @'
+$fileTemplate = @'
 # example input
 $in = @"
 "@
@@ -25,5 +7,33 @@ $in = @"
 # Get-Content -Path .\input.txt
 
 '@
+
+$parentDirectoryName = (Split-Path $pwd -Leaf)
+if ($parentDirectoryName -notmatch "^adventofcode\d{4}$") {
+    Write-Error "Parent dir must be named 'adventofcodeYYYY' where YYYY holds a number indicating the year."
+}
+
+for ($i = 1; $i -le 25; $i++) {
+    $day = "day$i"
+
+    $folderToCreate = Join-Path -Path $pwd -ChildPath $day
+    $null = New-Item -Path $folderToCreate -ItemType Directory -ErrorAction SilentlyContinue
+
+    $filesToCreate = "$day-1.ps1", "$day-2.ps1", "input.txt" | 
+        ForEach-Object { 
+            Join-Path -Path $pwd -ChildPath (
+                Join-Path -Path $day -ChildPath $_
+            )
+        }
+
+    $filesToCreate | 
+        ForEach-Object {
+            $null = New-Item -Path $_ -ItemType File -ErrorAction SilentlyContinue
+        }
+
+    $fileContent = Get-Content $filesToCreate[0]
+
+    if ([string]::IsNullOrEmpty($fileContent)) {
+        $null = Set-Content -Path $filesToCreate[0] -Value $fileTemplate
     }
 }

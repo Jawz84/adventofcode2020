5f5b3216506f96193e0a689640d71cd2a9291a6c
Jos Koelewijn <jos.koelewijn@cloudnation.nl>
2020-12-03 13:20:21 +0100

    day 3 solution

--- day3/day3-1.ps1
+++ day3/day3-1.ps1
@@ -1,7 +1,49 @@
 # example input
-$in = @"
-"@
+$in = Get-Content -Path .\testinput.txt | Where-Object {$_ -notlike ""}
 
 # puzzle input
-# Get-Content -Path .\input.txt
+$in = Get-Content -Path .\input.txt | Where-Object {$_ -notlike ""}
 
+$right = 3
+$down = 1
+
+
+function readinput {
+    param(
+        [string[]]$in
+    )
+
+    $list = New-Object System.Collections.ArrayList 
+    foreach ($i in $in) {
+        $null = $list.Add($i.ToCharArray())
+    }
+    return $list
+}
+
+function rideTobbogan {
+    param(
+        $right, $down, $map
+    )
+
+    $height = $map.count 
+    $width = $map[0].count
+
+    $x = 0
+
+    for ($y = 1; $y -lt $height; $y = $y + $down) {
+
+        $x = $x + $right
+        $wrappedX = $x % $width
+        $char = $map[$y][($wrappedX)]
+        
+        write-host "$y, $x ($wrappedX) `t: $char"
+        
+        if ($char -eq '#') {
+            1
+        }
+    }
+}
+
+$map = readinput -in $in
+
+(rideTobbogan -right $right -down $down -map $map).count
---
+++ day3/day3-2.ps1
@@ -0,0 +1,56 @@
+# example input
+$in = Get-Content -Path .\testinput.txt | Where-Object {$_ -notlike ""}
+
+# puzzle input
+$in = Get-Content -Path .\input.txt | Where-Object {$_ -notlike ""}
+
+$right = 3
+$down = 1
+
+
+function readinput {
+    param(
+        [string[]]$in
+    )
+
+    $list = New-Object System.Collections.ArrayList 
+    foreach ($i in $in) {
+        $null = $list.Add($i.ToCharArray())
+    }
+    return $list
+}
+
+function rideTobbogan {
+    param(
+        $right, $down, $map
+    )
+
+    $height = $map.count 
+    $width = $map[0].count
+
+    $x = 0
+
+    for ($y = $down; $y -lt $height; $y = $y + $down) {
+
+        $x = $x + $right
+        $wrappedX = $x % $width
+        $char = $map[$y][($wrappedX)]
+        
+        write-verbose "$y, $x ($wrappedX) `t: $char"
+        
+        if ($char -eq '#') {
+            1
+        }
+    }
+}
+
+$map = readinput -in $in
+
+($a = (rideTobbogan -right 1 -down 1 -map $map).count)
+($b = (rideTobbogan -right 3 -down 1 -map $map).count)
+($c = (rideTobbogan -right 5 -down 1 -map $map).count)
+($d = (rideTobbogan -right 7 -down 1 -map $map).count)
+($e = (rideTobbogan -right 1 -down 2 -map $map).count)
+
+# 2711882160 too low
+$a*$b*$c*$d*$e

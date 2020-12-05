3f12e546b619c2db9a0587136f27a176c8149e10
Jos Koelewijn <jos.koelewijn@cloudnation.nl>
2020-12-05 07:11:07 +0100

    Add solution day 5

--- day5/day5-1.ps1
+++ day5/day5-1.ps1
@@ -4,5 +4,73 @@
 $in = Get-Content -Path .\exampleinput.txt
 
 # puzzle input
-# $in = Get-Content -Path .\input.txt
+$in = Get-Content -Path .\input.txt
 
+function getSeatID {
+    param($row, $column)
+
+    return $row * 8 + $column
+}
+
+function getInput {
+    param($in)
+
+    foreach ($line in $in) {
+        if ($line -like "") {
+            continue 
+        }
+
+        [pscustomobject]@{
+            row    = $line.substring(0, 7)
+            column = $line.Substring(7, 3)
+        }
+    }
+}
+
+function decodeBoardingPass {
+    param($boardingpasses)
+
+    foreach ($pass in $boardingpasses) {
+        $row = decode -topChar 'B' -bottomChar 'F' -string $pass.row
+        $column = decode -topChar 'R' -bottomChar 'L' -string $pass.column
+        [pscustomobject]@{
+            row    = $row
+            column = $column
+            seatID = getSeatID -row $row -column $column
+        }
+    }
+}
+
+function decode {
+    param(
+        [char]$topChar,
+        [char]$bottomChar,
+        [string]$string
+    )
+    $steps = $string.Length
+    $upperBound = [math]::Pow(2, $steps)
+
+    $top = $upperBound
+    $bottom = 0
+    $chars = $string.ToCharArray()
+    for ($i = 0; $i -lt $steps; $i++) {
+        if ($chars[$i] -eq $topChar) {
+            $bottom = $bottom + ( $top - $bottom ) / 2
+        }
+        elseif ($chars[$i] -eq $bottomChar) {
+            $top = $top - ( $top - $bottom ) / 2
+        }
+    }
+    if ($chars[$i + 1] -eq $topChar) {
+        $top
+    }
+    else {
+        $bottom
+    }
+}
+
+$boardingpasses = getInput -in $in
+
+$decoded = decodeBoardingPass -boardingpasses $boardingpasses
+
+$decoded.seatID | Sort-Object -Descending | Select-Object -First 1

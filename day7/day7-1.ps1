c9c28c640d5cf833ad63e3ce1bc1e8789a8c90c1
Jos Koelewijn <jos.koelewijn@cloudnation.nl>
2020-12-07 20:47:21 +0100

    solution day 7 part 1

--- day7/day7-1.ps1
+++ day7/day7-1.ps1
@@ -4,5 +4,45 @@
 $in = Get-Content -Path .\exampleinput.txt
 
 # puzzle input
-# $in = Get-Content -Path .\input.txt
+$in = Get-Content -Path .\input.txt
 
+function readInput {
+    param($in)
+
+    foreach ($rule in $in) {
+        $res = ($rule -split "bags?,?" -replace "contain", "" | Where-Object {$_ -ne '.'} ).Trim()
+        $contents = $res | Select-Object -Skip 1 | Where-Object {$_ -notmatch "no other"}
+
+        $contents = $contents | ForEach-Object {
+            $t = $_ -split "(\d+)"
+            [PSCustomObject]@{
+                Name = $t[2].trim()
+                Amount = $t[1].trim()
+            }
+        }
+
+        [PSCustomObject]@{
+            Name     = $res[0]
+            contents = $contents
+        }
+    }
+}
+
+function findNested {
+    param(
+        $rules,
+        $target
+    )
+
+    $rule = $rules | Where-Object {$_.contents.Name -contains $target} 
+
+    foreach ($name in $rule.Name) {
+        $name
+        findNested -rules $rules -target $name
+    }
+}
+
+
+$rules = readInput -in $in
+
+(findNested -rules $rules -target 'shiny gold' | Sort-Object -Unique).Length

fbb05164499bc8e17a5b15ed3bdfc9ffd35f4675
Jos Koelewijn <joskw@live.com>
2020-12-14 10:40:29 +0100

    solution day 14, part 1

--- day14/day14-1.ps1
+++ day14/day14-1.ps1
@@ -4,5 +4,52 @@
 $in = Get-Content -Path .\exampleinput.txt
 
 # puzzle input
-# $in = Get-Content -Path .\input.txt
+$in = Get-Content -Path .\input.txt
 
+$in = $in | Where-Object { $_ -notlike "" }
+
+
+function toPaddedBinary {
+    param([double]$num)
+
+    $bin = [convert]::ToString($num, 2)
+    return '0' * (36 - $bin.Length) + $bin
+}
+
+$mem = @{}
+
+foreach ($i in $in) {
+    if ($i -match "mask = (?'mask'\S+)") {
+        $mask = $matches.mask
+    }
+    if ($i -match "mem\[(?'address'\d+)\] = (?'value'\d+)") {
+        $address = $matches.address
+        $value = $matches.value
+
+        $valueLong = toPaddedBinary $value
+
+        Write-Verbose $mask
+        Write-Verbose $valueLong
+
+        [double]$sum = 0.0
+        for ($i = 0; $i -lt 36; $i++ ) { 
+
+            if ($mask[$i] -eq '1') {
+                $sum += [Math]::Pow(2, 35 - $i)
+            }
+            elseif ($mask[$i] -ne '0' -and $valueLong[$i] -eq '1') {
+                $sum += [Math]::Pow(2, 35 - $i) 
+            }
+            
+            $longSum = toPaddedBinary $sum
+            if ($longSum -ne "000000000000000000000000000000000000") {
+                Write-Verbose $longSum
+            }
+        }
+        $mem.$address = $sum
+    }
+}
+
+[double]$sum = 0.0
+$mem.Values | ForEach-Object { $sum += $_ }
+$sum
